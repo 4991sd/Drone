fafbdb8ef60944665f6d2d2d97b7e16294ad6903
4991sd <102261109+4991sd@users.noreply.github.com>
2022-04-09 16:00:57 -0700

    Add files via upload

---
+++ DroneBT.ino
@@ -0,0 +1,32 @@
+#define Rxd 16
+#define Txd 17
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+ SerialBT.begin("David's Drone"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  Serial2.begin(9600, SERIAL_8N1, Rxd, Txd);
+}
+
+void loop() {
+ 
+ if (SerialBT.available()) {
+int  Message = SerialBT.read();
+     Serial.write(Message);
+     Serial2.write(Message);
+     delay(20);
+  }
+//    Serial.write(SerialBT.read());
+ //     Serial.write("hello");
+//      Serial.println("hello");
+//      Serial2.write("hello");
+//    Serial2.write(SerialBT.read());
+
+}
